8249a09b5bed8a8be6e42e24f72af54298333435
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-08-31 23:46:55 -0300

    Class 3

---
+++ Capgemini_exercise/aula_3.py
@@ -0,0 +1,15 @@
+# Laço de repetição
+
+idade_Marcelo = 1.20
+idade_Joao = 1.05
+idade = 8
+
+ 
+
+while idade_Joao <= idade_Marcelo:
+    idade_Marcelo + 0.05
+    idade_Joao + 0.07
+    idade += 1
+else:
+    print("Ei, Você ultrapassou o Marcelo")
+    print (f"Idade do João é{idade_Joao}")
