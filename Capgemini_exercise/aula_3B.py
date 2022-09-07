027ce93e88ddc520d4346f39e58f8ab3cd63434a
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-09-07 00:02:55 -0300

    Class 3

--- Capgemini_exercise/aula_3B.py
+++ Capgemini_exercise/aula_3B.py
@@ -1,14 +1,38 @@
-# Enquanto / While
+# # Enquanto / While
 
-number = input("Digite um número: \n")
-while number > 0:
-    number = number -1
+# number = input("Digite um número: \n")
+# while number > 0:
+#     number = number -1
+#     print(number)
+
+# num = 100
+# cont_pares = 0
+# while num <= 200:
+#     if num % 2 == 0:
+#         cont_pares = cont_pares + 1
+#     num = num + 1
+# print(cont_pares)
+
+
+
+# -------------------------------------------
+
+number = input("Digite um número entre 1 a 100\n")
+
+while number <= 100:
     print(number)
+    number += 1
 
-num = 100
-cont_pares = 0
-while num <= 200:
-    if num % 2 == 0:
-        cont_pares = cont_pares + 1
-    num = num + 1
-print(cont_pares)
+
+# -----------------------------------------------------------------
+# numero=1
+# max = int(input("Digite um inteiro maior que 1: ") )
+
+# print("Numeros pares entre 1 e", max, ":")
+
+# while numero <= max:
+#     if numero%2==0:
+#         print(numero,  end=" ")
+#     numero+=1
+
+# O end=" " é para mostrar um espaço em branco após imprimir cada número, ao invés de quebra de linha (enter).
