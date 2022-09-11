3289e63cbdaa0648b3204df542fbf35ecc162c4b
Juliana Navarro <juliananavarro2006@yahoo.com.br>
2022-09-11 18:03:49 -0300

    Class 3

--- Capgemini_exercise/aula_3.py
+++ Capgemini_exercise/aula_3.py
@@ -15,7 +15,8 @@ idade_Joao = 8
         #print(f"João voê tem {altura_Joao}, com {idade_Joao} anos, finalmente você passou o Marcelo")
 
 while altura_Joao < altura_Marcelo:
-    altura_Marcelo =  altura_Marcelo + 0.05
-    altura_Joao = altura_Joao + 0.07
+    altura_Marcelo = round(altura_Marcelo + 0.05,3)
+    altura_Joao = round(altura_Joao + 0.07,3)
     idade_Joao += 1
-print(idade_Joao)
+
+print(f"João voê tem {altura_Joao}, e com {idade_Joao} anos, finalmente você passou o Marcelo.")
